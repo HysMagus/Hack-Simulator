159e08999a34f662a50af0c8d6cbbb051be4ee9f
HysMagus <HysMagi116@gmail.com>
2016-05-13 15:46:41 -0500

    Add New Input Paths #1
    This adds 
    - ls and help() file paths

--- game.py
+++ game.py
@@ -14,6 +14,22 @@ while True:
 
     if usr == "root" and pwd == "supervisor":
         io("Welcome back!\n")
+        io("Terminal: " )
+        TermInp = input()
+        if TermInp == "ls" or TermInp == "help()":          
+            io("E:/ , Documents, Downloads, Pictures, P0rn")
+            io("Terminal:\n")
+            TerminalInp1 = input()
+            if TerminalInp1 == "cd E:/":
+                    io("ERROR USB is PASSWORD PROTECTED USE E:/ -pass:PASSWORD to access")
+            if TerminalInp1 == "Documents":
+                    io("Taco.txt")
+            if TerminalInp1 == "Downloads":
+                    io("UnicornPr0n")
+            if TerminalInp1 == "Pictures":
+                    io("NO FILES")
+            if TerminalInp1 == "P0rn":
+                    io("Giggity")            
         break
     else:
         io("Invalid user/password!\n")
